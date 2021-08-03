4278544ec26a318ef6b27db4a1d81d2a832a9ad4
tobyaskew1 <46008253+tobyaskew1@users.noreply.github.com>
2021-08-03 17:30:13 +0100

    Upload original
    This is the original upload to the collatz conjecture

---
+++ Collatz Conjecture/Collatz Conjecture/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Collatz_Conjecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            int counter = 0;
+
+            while (x >= 1)
+            {
+                while (x != 1)
+                {
+                    int y = x % 2;
+                    if (y == 0)
+                    {
+                        x = x / 2;
+                    }
+                    else
+                    {
+                        x = x * 3 + 1;
+                    }
+                    Console.WriteLine(x);
+                    counter++;
+                }
+                x++;
+            }
+            Console.WriteLine("It took {0} times!", counter);
+            Console.WriteLine(x);
+
+
+        }
+    }
+}
